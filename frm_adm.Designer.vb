df1a6df45347ad1dd67d54bafe0cfc4a4605fbf8
Arthur Alves Valentin <valentinarthuralves@gmail.com>
2021-11-17 20:49:29 -0300

    upd screen adm

--- frm_adm.Designer.vb
+++ frm_adm.Designer.vb
@@ -24,15 +24,15 @@ Partial Class frm_adm
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_adm))
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btn_logout = New System.Windows.Forms.Button()
+        Me.btn_salesman = New System.Windows.Forms.Button()
+        Me.btn_inventory = New System.Windows.Forms.Button()
+        Me.btn_copyorder = New System.Windows.Forms.Button()
+        Me.btn_seeorders = New System.Windows.Forms.Button()
+        Me.btn_dashboard = New System.Windows.Forms.Button()
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.btn_dashboard = New System.Windows.Forms.Button()
-        Me.btn_seeorders = New System.Windows.Forms.Button()
-        Me.btn_copyorder = New System.Windows.Forms.Button()
-        Me.btn_inventory = New System.Windows.Forms.Button()
-        Me.btn_salesman = New System.Windows.Forms.Button()
-        Me.btn_logout = New System.Windows.Forms.Button()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -41,7 +41,7 @@ Partial Class frm_adm
         '
         'Panel1
         '
-        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(49, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(168, Byte), Integer))
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(92, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(188, Byte), Integer))
         Me.Panel1.Controls.Add(Me.btn_logout)
         Me.Panel1.Controls.Add(Me.btn_salesman)
         Me.Panel1.Controls.Add(Me.btn_inventory)
@@ -53,6 +53,60 @@ Partial Class frm_adm
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(205, 460)
         Me.Panel1.TabIndex = 1
+        '
+        'btn_logout
+        '
+        Me.btn_logout.Location = New System.Drawing.Point(12, 425)
+        Me.btn_logout.Name = "btn_logout"
+        Me.btn_logout.Size = New System.Drawing.Size(75, 23)
+        Me.btn_logout.TabIndex = 5
+        Me.btn_logout.Text = "Button6"
+        Me.btn_logout.UseVisualStyleBackColor = True
+        '
+        'btn_salesman
+        '
+        Me.btn_salesman.Location = New System.Drawing.Point(0, 324)
+        Me.btn_salesman.Name = "btn_salesman"
+        Me.btn_salesman.Size = New System.Drawing.Size(205, 32)
+        Me.btn_salesman.TabIndex = 4
+        Me.btn_salesman.Text = "Button5"
+        Me.btn_salesman.UseVisualStyleBackColor = True
+        '
+        'btn_inventory
+        '
+        Me.btn_inventory.Location = New System.Drawing.Point(0, 276)
+        Me.btn_inventory.Name = "btn_inventory"
+        Me.btn_inventory.Size = New System.Drawing.Size(205, 32)
+        Me.btn_inventory.TabIndex = 3
+        Me.btn_inventory.Text = "Button4"
+        Me.btn_inventory.UseVisualStyleBackColor = True
+        '
+        'btn_copyorder
+        '
+        Me.btn_copyorder.Location = New System.Drawing.Point(0, 228)
+        Me.btn_copyorder.Name = "btn_copyorder"
+        Me.btn_copyorder.Size = New System.Drawing.Size(205, 32)
+        Me.btn_copyorder.TabIndex = 2
+        Me.btn_copyorder.Text = "Button3"
+        Me.btn_copyorder.UseVisualStyleBackColor = True
+        '
+        'btn_seeorders
+        '
+        Me.btn_seeorders.Location = New System.Drawing.Point(0, 180)
+        Me.btn_seeorders.Name = "btn_seeorders"
+        Me.btn_seeorders.Size = New System.Drawing.Size(205, 32)
+        Me.btn_seeorders.TabIndex = 1
+        Me.btn_seeorders.Text = "Button2"
+        Me.btn_seeorders.UseVisualStyleBackColor = True
+        '
+        'btn_dashboard
+        '
+        Me.btn_dashboard.Location = New System.Drawing.Point(0, 132)
+        Me.btn_dashboard.Name = "btn_dashboard"
+        Me.btn_dashboard.Size = New System.Drawing.Size(205, 32)
+        Me.btn_dashboard.TabIndex = 0
+        Me.btn_dashboard.Text = "Button1"
+        Me.btn_dashboard.UseVisualStyleBackColor = True
         '
         'Panel2
         '
@@ -85,60 +139,6 @@ Partial Class frm_adm
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
-        '
-        'btn_dashboard
-        '
-        Me.btn_dashboard.Location = New System.Drawing.Point(0, 132)
-        Me.btn_dashboard.Name = "btn_dashboard"
-        Me.btn_dashboard.Size = New System.Drawing.Size(205, 32)
-        Me.btn_dashboard.TabIndex = 0
-        Me.btn_dashboard.Text = "Button1"
-        Me.btn_dashboard.UseVisualStyleBackColor = True
-        '
-        'btn_seeorders
-        '
-        Me.btn_seeorders.Location = New System.Drawing.Point(0, 180)
-        Me.btn_seeorders.Name = "btn_seeorders"
-        Me.btn_seeorders.Size = New System.Drawing.Size(205, 32)
-        Me.btn_seeorders.TabIndex = 1
-        Me.btn_seeorders.Text = "Button2"
-        Me.btn_seeorders.UseVisualStyleBackColor = True
-        '
-        'btn_copyorder
-        '
-        Me.btn_copyorder.Location = New System.Drawing.Point(0, 228)
-        Me.btn_copyorder.Name = "btn_copyorder"
-        Me.btn_copyorder.Size = New System.Drawing.Size(205, 32)
-        Me.btn_copyorder.TabIndex = 2
-        Me.btn_copyorder.Text = "Button3"
-        Me.btn_copyorder.UseVisualStyleBackColor = True
-        '
-        'btn_inventory
-        '
-        Me.btn_inventory.Location = New System.Drawing.Point(0, 276)
-        Me.btn_inventory.Name = "btn_inventory"
-        Me.btn_inventory.Size = New System.Drawing.Size(205, 32)
-        Me.btn_inventory.TabIndex = 3
-        Me.btn_inventory.Text = "Button4"
-        Me.btn_inventory.UseVisualStyleBackColor = True
-        '
-        'btn_salesman
-        '
-        Me.btn_salesman.Location = New System.Drawing.Point(0, 324)
-        Me.btn_salesman.Name = "btn_salesman"
-        Me.btn_salesman.Size = New System.Drawing.Size(205, 32)
-        Me.btn_salesman.TabIndex = 4
-        Me.btn_salesman.Text = "Button5"
-        Me.btn_salesman.UseVisualStyleBackColor = True
-        '
-        'btn_logout
-        '
-        Me.btn_logout.Location = New System.Drawing.Point(12, 425)
-        Me.btn_logout.Name = "btn_logout"
-        Me.btn_logout.Size = New System.Drawing.Size(75, 23)
-        Me.btn_logout.TabIndex = 5
-        Me.btn_logout.Text = "Button6"
-        Me.btn_logout.UseVisualStyleBackColor = True
         '
         'frm_adm
         '
