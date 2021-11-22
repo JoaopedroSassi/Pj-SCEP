ad56a04e087083af293b7178486f27f727a342f8
João Pedro Sassi Granado <joao.granado@fatec.sp.gov.br>
2021-11-22 09:29:24 -0300

    Upt date doc

--- frm_adm.Designer.vb
+++ frm_adm.Designer.vb
@@ -24,9 +24,6 @@ Partial Class frm_adm
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_adm))
-        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
-        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
-        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
         Me.pic_logo = New System.Windows.Forms.PictureBox()
         Me.btn_prod = New FontAwesome.Sharp.IconButton()
         Me.btn_seller = New FontAwesome.Sharp.IconButton()
@@ -36,7 +33,6 @@ Partial Class frm_adm
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.pnl_content = New System.Windows.Forms.Panel()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
         Me.TbproductsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.Db_scepDataSet = New scep.db_scepDataSet()
         Me.Tb_productsTableAdapter = New scep.db_scepDataSetTableAdapters.tb_productsTableAdapter()
@@ -44,7 +40,6 @@ Partial Class frm_adm
         Me.pnl_menu.SuspendLayout()
         Me.Panel1.SuspendLayout()
         Me.pnl_content.SuspendLayout()
-        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TbproductsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Db_scepDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -187,42 +182,6 @@ Partial Class frm_adm
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "admin"
         '
-        'Chart1
-        '
-        ChartArea1.AxisX.Title = "Produtos"
-        ChartArea1.AxisY.Title = "Quantidade"
-        ChartArea1.Name = "ChartArea1"
-        Me.Chart1.ChartAreas.Add(ChartArea1)
-        Me.Chart1.DataSource = Me.TbproductsBindingSource
-        Legend1.Name = "Legend1"
-        Me.Chart1.Legends.Add(Legend1)
-        Me.Chart1.Location = New System.Drawing.Point(412, 146)
-        Me.Chart1.Name = "Chart1"
-        Me.Chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel
-        Series1.BackImageTransparentColor = System.Drawing.Color.White
-        Series1.BackSecondaryColor = System.Drawing.Color.White
-        Series1.BorderColor = System.Drawing.Color.White
-        Series1.ChartArea = "ChartArea1"
-        Series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn
-        Series1.Color = System.Drawing.Color.White
-        Series1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
-        Series1.IsValueShownAsLabel = True
-        Series1.LabelBackColor = System.Drawing.Color.Transparent
-        Series1.LabelBorderColor = System.Drawing.Color.Transparent
-        Series1.LabelForeColor = System.Drawing.Color.White
-        Series1.Legend = "Legend1"
-        Series1.MarkerBorderColor = System.Drawing.Color.White
-        Series1.MarkerColor = System.Drawing.Color.White
-        Series1.Name = "Series1"
-        Series1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale
-        Series1.XValueMember = "name"
-        Series1.YValueMembers = "amount"
-        Series1.YValuesPerPoint = 2
-        Me.Chart1.Series.Add(Series1)
-        Me.Chart1.Size = New System.Drawing.Size(473, 278)
-        Me.Chart1.TabIndex = 1
-        Me.Chart1.Text = "Chart1"
-        '
         'TbproductsBindingSource
         '
         Me.TbproductsBindingSource.DataMember = "tb_products"
@@ -243,7 +202,6 @@ Partial Class frm_adm
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(1101, 511)
-        Me.Controls.Add(Me.Chart1)
         Me.Controls.Add(Me.pnl_content)
         Me.Controls.Add(Me.pnl_menu)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
@@ -256,7 +214,6 @@ Partial Class frm_adm
         Me.Panel1.ResumeLayout(False)
         Me.pnl_content.ResumeLayout(False)
         Me.pnl_content.PerformLayout()
-        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TbproductsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Db_scepDataSet, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
@@ -272,7 +229,6 @@ Partial Class frm_adm
     Friend WithEvents Panel1 As Windows.Forms.Panel
     Friend WithEvents pnl_content As Windows.Forms.Panel
     Friend WithEvents Label1 As Windows.Forms.Label
-    Friend WithEvents Chart1 As Windows.Forms.DataVisualization.Charting.Chart
     Friend WithEvents Db_scepDataSet As db_scepDataSet
     Friend WithEvents TbproductsBindingSource As Windows.Forms.BindingSource
     Friend WithEvents Tb_productsTableAdapter As db_scepDataSetTableAdapters.tb_productsTableAdapter
